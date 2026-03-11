1dab16f72855a9552bc70db8985f4619a49c91a8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-11 19:14:34 +0530

    Updated

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,12 @@
 [
-  import_deps: [:stream_data],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  line_length: 100,
+  line_length: 120,
   locals_without_parens: [
-    # Custom formatting for TinkEx
+    # Ecto-style field declarations (used in type structs)
     field: 2,
-    field: 3
+    field: 3,
+
+    # defdelegate without parens on target
+    defdelegate: 2
   ]
 ]
