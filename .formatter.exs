aabc8da9c38edbd33889bc236cfe4cba95e36390
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-06-18 18:08:26 +0530

    updated

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,4 @@
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  line_length: 120,
-  locals_without_parens: [
-    # Ecto-style field declarations (used in type structs)
-    field: 2,
-    field: 3,
-
-    # defdelegate without parens on target
-    defdelegate: 2
-  ]
+  line_length: 100
 ]
