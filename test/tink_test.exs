210a889692738eb16f300f95ae6ef361d71462b1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-06-18 18:14:50 +0530

    Updated

--- test/tink_test.exs
+++ test/tink_test.exs
@@ -1,8 +1,24 @@
 defmodule TinkTest do
-  use ExUnit.Case
-  doctest Tink
+  use ExUnit.Case, async: true
+  import Mox
+  import Tink.TestHelpers
+  setup :verify_on_exit!
 
-  test "greets the world" do
-    assert Tink.hello() == :world
+  test "app_client/2 delegates to Auth.client_credentials" do
+    expect(Tink.HTTP.Mock, :request, fn :post, _url, _h, body_str, _o ->
+      assert String.contains?(body_str, "grant_type=client_credentials")
+      {:ok, %{status: 200, headers: [], body: Jason.encode!(token_response())}}
+    end)
+
+    assert {:ok, %Tink.Client{}} = Tink.app_client("authorization:grant")
+  end
+
+  test "user_client/2 delegates to Auth.user_client" do
+    expect(Tink.HTTP.Mock, :request, fn :post, _url, _h, body_str, _o ->
+      assert String.contains?(body_str, "grant_type=authorization_code")
+      {:ok, %{status: 200, headers: [], body: Jason.encode!(token_response())}}
+    end)
+
+    assert {:ok, %Tink.Client{}} = Tink.user_client("redirect_code")
   end
 end
